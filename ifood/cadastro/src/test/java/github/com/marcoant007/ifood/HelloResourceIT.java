633500dcaa1af9cbe8017221fb328bc390dafb78
Marcoant007 <marcoantnovo@gmail.com>
2022-08-31 00:05:59 -0300

    add testingContainer with database rider

--- ifood/cadastro/src/test/java/github/com/marcoant007/ifood/HelloResourceIT.java
+++
@@ -1,8 +0,0 @@
-package github.com.marcoant007.ifood;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HelloResourceIT extends HelloResourceTest {
-    // Execute the same tests but in packaged mode.
-}
